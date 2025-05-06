74a0f4aafcb6a1e0259ef4c5029942c6037113f9
atria10 <atriamario.10@gmail.com>
2025-05-06 12:19:25 +0200

    created Typography section

--- src/components/Colors.tsx
+++ src/components/Colors.tsx
@@ -1,6 +1,6 @@
 const Colors = () => {
   return (
-    <section id="colors" class="bg-dark text-white" style="padding:1rem 2rem;">
+    <section id="colors" class="bg-dark text-white" style="margin:4rem 0; padding:1rem 2rem;">
       <h2 class="uppercase letter-spacing-1">
         <span class="text-accent">01</span > colors
       </h2>
--- src/components/Typography.tsx
+++ src/components/Typography.tsx
@@ -1,5 +1,76 @@
 const Typography = () => {
-  return <div>Typography</div>;
+  return (
+    <section id="typoghraphy" style="margin:4rem 0;">
+      <h2 class="uppercase letter-spacing-1">
+        <span class="text-accent">02</span> typography
+      </h2>
+      <div class="flex" style="--gap: 5rem; width: 100%; margin:2rem 0">
+        <div style="flex-basis:100%">
+          <div>
+            <p class="text-accent">Heading 1 - Bellefair Regular - 150px</p>
+            <p class="fs-900 ff-serif uppercase">Earth</p>
+          </div>
+          <div class="text-accent">
+            <p>Heading 2 - Bellefair Regular - 100px</p>
+            <p class="fs-800 ff-serif uppercase">Venus</p>
+          </div>
+          <div class="text-accent">
+            <p>Heading 3 - Bellefair Regular - 56px</p>
+            <p class="fs-700 ff-serif uppercase">Jupiter & Saturn</p>
+          </div>
+          <div />
+          <div>
+            <p class="text-accent">Heading 4 - Bellefair Regular - 32px</p>
+            <p class="fs-600 ff-serif uppercase">Uranus, Neptune & Pluto</p>
+          </div>
+          <div>
+            <p class="text-accent ">
+              Heading 5 - Bellefair Condensed Regular - 28px - 4.75 Character
+              Space
+            </p>
+            <p class="text-accent fs-500 ff-sans-cond uppercase letter-spacing-1">So, you want to travel space</p>
+          </div>
+        </div>
+        <div style={{ "flex-basis": "100%" }}>
+          <div>
+            <p class="text-accent">Subheading 1 - Bellefair Regular - 28px</p>
+            <p class="fs-500 ff-serif uppercase">384,400 km</p>
+          </div>
+          <div>
+            <p class="text-accent">
+              Subheading 2 - Barlow Condensed Regular- 14px - 2.35 Character
+              Space
+            </p>
+            <p class="fs-200 ff-sans-cond uppercase letter-spacing-3">Avg. Distance</p>
+          </div>
+          <div>
+            <p class="text-accent">
+              Nav Text - Barlow Condensed Regular - 16px - 2.7 Character Space
+            </p>
+            <p class="fs-300  uppercase ff-sans-cond letter-spacing-2">Europa</p>
+          </div>
+
+          <div>
+            <p class="text-accent">Body Text</p>
+            <p class="">
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quae
+            dignissimos modi quam necessitatibus obcaecati corporis, voluptas
+            quia mollitia, delectus recusandae doloremque repudiandae provident
+            corrupti amet temporibus deleniti facilis cum. Maxime! Lorem ipsum
+            dolor sit amet, consectetur adipisicing elit. Quae dignissimos modi
+            quam necessitatibus obcaecati corporis, voluptas quia mollitia,
+            delectus recusandae doloremque repudiandae provident corrupti amet
+            temporibus deleniti facilis cum. Maxime! Lorem ipsum dolor sit amet,
+            consectetur adipisicing elit. Quae dignissimos modi quam
+            necessitatibus obcaecati corporis, voluptas quia mollitia, delectus
+            recusandae doloremque repudiandae provident corrupti amet temporibus
+            deleniti facilis cum. Maxime!
+            </p>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default Typography;
