321537da3bb418453d527ff547ba97e3d9de1698
atria10 <atriamario.10@gmail.com>
2025-05-09 14:53:03 +0200

    feat: completed crew page --> made it responsive for all media queries

--- src/components/NumberedTitle.tsx
+++ src/components/NumberedTitle.tsx
@@ -5,10 +5,7 @@ type Props = {
 
 const NumberedTitle = ({ number, title }: Props) => {
   return (
-    <h2
-      class={`text-sans-cond text-lg uppercase tracking-wider
- text-center text-white`}
-    >
+    <h2 class={`text-sans-cond uppercase tracking-wider text-white`}>
       <span class="text-[hsl(0_0%_100%_/_25%)] mr-0.5">{number}</span> {title}
     </h2>
   );
--- src/pages/Crew.tsx
+++ src/pages/Crew.tsx
@@ -17,17 +17,29 @@ export const Crew = () => {
   };
 
   return (
-    <div class={` grid ${smallGridArea} gap-2 items-start`}>
-      <div class="[grid-area:title]">
+    <div
+      class={` grid ${smallGridArea} ${tabletGridArea} ${largeGridArea} gap-2`}
+    >
+      <div
+        class="[grid-area:title] text-center md:text-left text-lg
+       md:text-2xl lg:text-3xl flex justify-center md:justify-start md:px-4 lg:px-[15%]"
+      >
         <NumberedTitle number={"02"} title="Meet your crew" />
       </div>
 
-      <main class="[grid-area:article] text-white text-center">
-        <h2 class="text-serif text-md text-[hsl(0_0%_100%_/_40%)] uppercase">
+      <main
+        class="[grid-area:article] self-center
+         lg:self-start text-white text-center lg:text-left flex flex-col lg:justify-start"
+      >
+        <h2 class="text-serif text-md md:text-2xl lg:text-3xl   text-[hsl(0_0%_100%_/_40%)] uppercase">
           {crew().role}
         </h2>
-        <h2 class="text-serif text-xl tracking-wider uppercase">{crew().name}</h2>
-        <p class="text-sm text-accent tracking-wide mt-2">{crew().bio}</p>
+        <h2 class="text-serif text-xl md:text-4xl lg:text-5xl tracking-wider lg:tracking-normal uppercase">
+          {crew().name}
+        </h2>
+        <p class="text-sm md:text-lg text-accent tracking-wide mt-2 md:my-6 md:w-2/3 lg:w-auto md:m-auto">
+          {crew().bio}
+        </p>
       </main>
 
       <section class="[grid-area:image] border-[hsl(0_0%_100%_/_20%)] border-b-1">
@@ -36,11 +48,12 @@ export const Crew = () => {
           <img
             src={crew().images.png}
             alt={crew().name}
-            class="max-w-[60%] mx-auto lg:max-w-[90%]"
+            class="max-w-[60%] mx-auto md:max-w-[50%] lg:max-w-[70%] lg:min-w-[60%]"
           />
         </picture>
       </section>
-      <section class="[grid-area:dots]">
+
+      <section class="[grid-area:dots] lg:justify-self-start lg:self-start">
         <Dots
           options={crews.map((item) => item.name)}
           onClick={handleCrew}
@@ -52,3 +65,7 @@ export const Crew = () => {
 };
 
 const smallGridArea = `[grid-template-areas:'title''image''dots''article']`;
+const tabletGridArea = `md:[grid-template-areas:'title''article''dots''image']`;
+const largeGridArea = `lg:[grid-template-areas:'title_title_image_image''article_._image_image''dots_dots_image_image']
+   lg:grid-cols-[minmax(0,_40%)_1fr_1fr_1fr] lg:px-[10vh]
+`;
